4d9016fc8ad1c0f4cb31e9eef50e50df8c35d9c7
chwerob <vminolas@gmail.com>
2025-05-14 07:47:28 +0200

    fix : problem during npm setup non execution

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,54 @@
-const taskStatusList = ["Pending", "In Progress", "Done"] as const; //as const makes taskStatusList a readonly tuple of exact string literals.
+const myArr = [7, 1, 4, 9, 8, 2, 5];
 
-type TaskStatus = (typeof taskStatusList)[number]; // typeof taskStatusList[number] extracts a union of the values: 'Pending' | 'In Progress' | 'Done'
+function getKeyByValue(map: Map<number, number>, searchValue: number) {
+  for (const [key, value] of map.entries()) {
+    if (value === searchValue) {
+      return key;
+    }
+  }
+  return null; // or undefined
+}
 
-const taskStatus: TaskStatus = "Pending";
-console.log(taskStatus);
-console.log(taskStatusList);
+const myMap = new Map<number, number>();
+
+console.log(myMap);
+
+console.log(getKeyByValue(myMap, 7));
+
+for (let i = 0; i < myArr.length; i++) {
+  for (let j = 0; j < myArr.length - 1; j++) {
+    if (myArr[j] < myArr[j + 1]) {
+      const temp = myArr[j + 1];
+      myArr[j + 1] = myArr[j];
+      myArr[j] = temp;
+    }
+  }
+}
+
+//console.log(myArr);
+
+function bubbleSortReverse(arr: number[]) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] < arr[j + 1]) {
+        const temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSortReverse(myArr));
+for (let i = 0; i < myArr.length; i++) {
+  myMap.set(i, myArr[i]);
+}
+
+for (let i = 0; i < myArr.length; i++) {
+  console.log(getKeyByValue(myMap, myArr[i]));
+}
+
+while (true) {
+  coole.log("Hello");
+}
